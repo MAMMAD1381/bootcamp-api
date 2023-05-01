9ba38b08ea996908cfc852960a39632ea429e3e9
MAMMAD1381 <81987793+MAMMAD1381@users.noreply.github.com>
2023-05-02 03:12:07 +0330

    adding a model for bootcamps for using in mongodb

--- configs/db.js
+++ configs/db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
+require('colors')
 async function connectDB(){
     const db_url = process.env.DB_URL
     const connection = await mongoose.connect(db_url)
 
-    console.log(`connected to host: ${connection.connection.host}`)
+    console.log(`connected to host: ${connection.connection.host}`.blue)
 }
 
 module.exports = connectDB
